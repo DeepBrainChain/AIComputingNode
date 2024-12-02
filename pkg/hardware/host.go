172f7fe47479353d5405824910490a884dba4d81
Jerry <1364812552@qq.com>
2024-12-02 15:23:17 +0800

    fix: ghw may encounter problems under macos

--- pkg/hardware/host.go
+++ pkg/hardware/host.go
@@ -12,57 +12,57 @@ import (
 func GetHostInfo() (*types.HostInfo, error) {
 	var reterr error = nil
 
+	hi := &types.HostInfo{}
 	hostInfo, err := psutil.Info()
 	if err != nil {
 		log.Logger.Warnf("Error retrieving OS info: %v", err)
 		reterr = err
-	}
-
-	hi := &types.HostInfo{
-		Os: types.OSInfo{
-			OS:              hostInfo.OS,
-			Platform:        hostInfo.Platform,
-			PlatformFamily:  hostInfo.PlatformFamily,
-			PlatformVersion: hostInfo.PlatformVersion,
-			KernelVersion:   hostInfo.KernelVersion,
-			KernelArch:      hostInfo.KernelArch,
-		},
+	} else {
+		hi.Os.OS = hostInfo.OS
+		hi.Os.Platform = hostInfo.Platform
+		hi.Os.PlatformFamily = hostInfo.PlatformFamily
+		hi.Os.PlatformVersion = hostInfo.PlatformVersion
+		hi.Os.KernelVersion = hostInfo.KernelVersion
+		hi.Os.KernelArch = hostInfo.KernelArch
 	}
 
 	cpu, err := ghw.CPU()
 	if err != nil {
 		log.Logger.Warnf("Error getting CPU info: %v", err)
 		reterr = err
-	}
-	for _, processor := range cpu.Processors {
-		hi.Cpu = append(hi.Cpu, types.CpuInfo{
-			ModelName: processor.Model,
-			Cores:     processor.NumCores,
-			Threads:   processor.NumThreads,
-		})
+	} else {
+		for _, processor := range cpu.Processors {
+			hi.Cpu = append(hi.Cpu, types.CpuInfo{
+				ModelName: processor.Model,
+				Cores:     processor.NumCores,
+				Threads:   processor.NumThreads,
+			})
+		}
 	}
 
 	memory, err := ghw.Memory()
 	if err != nil {
 		log.Logger.Warnf("Error getting memory info: %v", err)
 		reterr = err
+	} else {
+		hi.Memory.TotalPhysicalBytes = memory.TotalPhysicalBytes
+		hi.Memory.TotalUsableBytes = memory.TotalUsableBytes
 	}
-	hi.Memory.TotalPhysicalBytes = memory.TotalPhysicalBytes
-	hi.Memory.TotalUsableBytes = memory.TotalUsableBytes
 
 	block, err := ghw.Block()
 	if err != nil {
 		log.Logger.Warnf("Error getting block storage info: %v", err)
 		reterr = err
-	}
-	for _, disk := range block.Disks {
-		if disk.StorageController != ghw_block.STORAGE_CONTROLLER_LOOP {
-			hi.Disk = append(hi.Disk, types.DiskInfo{
-				DriveType:    disk.DriveType.String(),
-				SizeBytes:    disk.SizeBytes,
-				Model:        disk.Model,
-				SerialNumber: disk.SerialNumber,
-			})
+	} else {
+		for _, disk := range block.Disks {
+			if disk.StorageController != ghw_block.STORAGE_CONTROLLER_LOOP {
+				hi.Disk = append(hi.Disk, types.DiskInfo{
+					DriveType:    disk.DriveType.String(),
+					SizeBytes:    disk.SizeBytes,
+					Model:        disk.Model,
+					SerialNumber: disk.SerialNumber,
+				})
+			}
 		}
 	}
 
@@ -70,23 +70,24 @@ func GetHostInfo() (*types.HostInfo, error) {
 	if err != nil {
 		log.Logger.Warnf("Error getting GPU info: %v", err)
 		reterr = err
-	}
-	for _, card := range gpu.GraphicsCards {
-		vendor := ""
-		product := ""
-		if card.DeviceInfo != nil {
-			if card.DeviceInfo.Vendor != nil {
-				vendor = card.DeviceInfo.Vendor.Name
+	} else {
+		for _, card := range gpu.GraphicsCards {
+			vendor := ""
+			product := ""
+			if card.DeviceInfo != nil {
+				if card.DeviceInfo.Vendor != nil {
+					vendor = card.DeviceInfo.Vendor.Name
+				}
+				if card.DeviceInfo.Product != nil {
+					product = card.DeviceInfo.Product.Name
+				}
 			}
-			if card.DeviceInfo.Product != nil {
-				product = card.DeviceInfo.Product.Name
+			if product != "" {
+				hi.Gpu = append(hi.Gpu, types.GpuInfo{
+					Vendor:  vendor,
+					Product: product,
+				})
 			}
-		}
-		if product != "" {
-			hi.Gpu = append(hi.Gpu, types.GpuInfo{
-				Vendor:  vendor,
-				Product: product,
-			})
 		}
 	}
 
--- tools/host/main.go
+++ tools/host/main.go
@@ -14,54 +14,55 @@ import (
 func main() {
 	cpu, err := ghw.CPU()
 	if err != nil {
-		fmt.Printf("Error getting CPU info: %v", err)
-	}
-	fmt.Printf("%v\n", cpu)
+		fmt.Printf("Error getting CPU info: %v\n", err)
+	} else {
+		fmt.Printf("%v\n", cpu)
 
-	for _, processor := range cpu.Processors {
-		fmt.Printf("CPU %v: %v (%d, %d)\n", processor.ID, processor.Model,
-			processor.NumCores, processor.NumThreads)
+		for _, processor := range cpu.Processors {
+			fmt.Printf("CPU %v: %v (%d, %d)\n", processor.ID, processor.Model,
+				processor.NumCores, processor.NumThreads)
+		}
 	}
 
 	memory, err := ghw.Memory()
 	if err != nil {
-		fmt.Printf("Error getting memory info: %v", err)
+		fmt.Printf("Error getting memory info: %v\n", err)
+	} else {
+		fmt.Println(memory.String())
 	}
-
-	fmt.Println(memory.String())
 
 	block, err := ghw.Block()
 	if err != nil {
-		fmt.Printf("Error getting block storage info: %v", err)
-	}
+		fmt.Printf("Error getting block storage info: %v\n", err)
+	} else {
+		fmt.Printf("%v\n", block)
 
-	fmt.Printf("%v\n", block)
-
-	for _, disk := range block.Disks {
-		fmt.Printf(" %v\n", disk)
-		for _, part := range disk.Partitions {
-			fmt.Printf("  %v\n", part)
+		for _, disk := range block.Disks {
+			fmt.Printf(" %v\n", disk)
+			for _, part := range disk.Partitions {
+				fmt.Printf("  %v\n", part)
+			}
 		}
 	}
 
 	gpu, err := ghw.GPU()
 	if err != nil {
-		fmt.Printf("Error getting GPU info: %v", err)
-	}
+		fmt.Printf("Error getting GPU info: %v\n", err)
+	} else {
+		fmt.Printf("%v\n", gpu)
 
-	fmt.Printf("%v\n", gpu)
-
-	for _, card := range gpu.GraphicsCards {
-		fmt.Printf(" %v\n", card)
+		for _, card := range gpu.GraphicsCards {
+			fmt.Printf(" %v\n", card)
+		}
 	}
 
 	hi, err := hardware.GetHostInfo()
 	if err != nil {
-		fmt.Printf("Error getting host info: %v", err)
+		fmt.Printf("Error getting host info: %v\n", err)
 	}
 	jsonData, err := json.MarshalIndent(hi, "", "  ")
 	if err != nil {
-		fmt.Printf("Error marshaling host json: %v", err)
+		fmt.Printf("Error marshaling host json: %v\n", err)
 		os.Exit(1)
 	}
 	fmt.Println(string(jsonData))
@@ -70,13 +71,14 @@ func main() {
 	if err != nil {
 		fmt.Printf("Error retrieving OS info: %v\n", err)
 		return
+	} else {
+		fmt.Printf("OS: %s\n", hostInfo.OS)
+		fmt.Printf("Platform: %s\n", hostInfo.Platform)
+		fmt.Printf("Platform Family: %s\n", hostInfo.PlatformFamily)
+		fmt.Printf("Platform Version: %s\n", hostInfo.PlatformVersion)
+		fmt.Printf("Kernel Version: %s\n", hostInfo.KernelVersion)
+		fmt.Printf("Kernel Arch: %s\n", hostInfo.KernelArch)
+		fmt.Printf("Virtualization System: %s\n", hostInfo.VirtualizationSystem)
+		fmt.Printf("Virtualization Role: %s\n", hostInfo.VirtualizationRole)
 	}
-	fmt.Printf("OS: %s\n", hostInfo.OS)
-	fmt.Printf("Platform: %s\n", hostInfo.Platform)
-	fmt.Printf("Platform Family: %s\n", hostInfo.PlatformFamily)
-	fmt.Printf("Platform Version: %s\n", hostInfo.PlatformVersion)
-	fmt.Printf("Kernel Version: %s\n", hostInfo.KernelVersion)
-	fmt.Printf("Kernel Arch: %s\n", hostInfo.KernelArch)
-	fmt.Printf("Virtualization System: %s\n", hostInfo.VirtualizationSystem)
-	fmt.Printf("Virtualization Role: %s\n", hostInfo.VirtualizationRole)
 }
